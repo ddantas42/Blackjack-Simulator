83ddbdfe00776a8b8f28f8d0edcf8475fcb4cfa6
Diogo <email>
2024-03-06 10:20:40 +0000

    ª

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int random_id() { return rand() % 13; }
 void print_table(Hand Player, Hand Dealer)
 {
 	system("clear");
-	std::cout << "Player's bet: " << Player.getBet() << std::endl;
+	std::cout << "Player's bet: " << Player.getBet() << "€" << std::endl;
 	std::cout << Dealer << std::endl;
 	std::cout << Player << std::endl;
 }
@@ -33,19 +33,17 @@ int main()
 
 		std::string choice;
 question:
-		std::cout << "Bankroll: " << Player.getBankroll() << std::endl;
+		std::cout << "Bankroll: " << Player.getBankroll() << "€" << std::endl;
 		std::cout << "Play again? (y/n) ";
 		std::cin >> choice;
 		if (choice == "n" || choice == "N")
 			break;
 		else if (choice == "y" || choice == "Y")
-			{
-				if (Player.getBankroll() <= 0)
-					std::cout << "You're out of money! Game over!" << std::endl;
-			sleep(1);
-			
-
-			}
+		{
+			if (Player.getBankroll() <= 0)
+				std::cout << "You're out of money! Game over!" << std::endl;
+		sleep(1);
+		}
 		else
 			goto question;
 	}
